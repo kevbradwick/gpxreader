f0c9e6d361685732ca56ba1204c33cb2f6aee58b
Kevin Bradwick <kevinbradwick@gmail.com>
2013-02-19 21:05:33 +0000

    refactored hear rate and cadence function to return multiple values

--- src/gpxreader/info.go
+++ src/gpxreader/info.go
@@ -8,11 +8,12 @@ import (
   "io/ioutil"
   "encoding/xml"
   "fmt"
+  "time"
 )
 
 type Gpx struct {
   Creator     string `xml:"creator,attr"`
-  Time        string `xml:"metadata>time"`
+  Time        time.Time `xml:"metadata>time"`
   Title       string `xml:"trk>name"`
   TrackPoints []TrackPoint `xml:"trk>trkseg>trkpt"`
 }
@@ -22,13 +23,19 @@ func (g Gpx) String() string {
 
   msg := `
 GPX Reader v0.1.0
-Creator:    %s
-Time:       %s
-Title:      %s
-Max HR:     %v
-Avg HR:     %v
+Creator:          %s
+Time:             %s
+Title:            %s
+Max HR:           %v
+Avg HR:           %v
+Total Elevation:  %v
+Cadence (Max):    %v
+Cadence (Avg):    %v
 `
-  return fmt.Sprintf(msg, g.Creator, g.Time, g.Title, g.MaxHeartRate(), g.AverageHeartRate())
+  cadAvg, cadMax := g.Cadence()
+  hrMax, hrAvg := g.HeartRate()
+  return fmt.Sprintf(msg, g.Creator, g.Time, g.Title, hrMax,
+    hrAvg, g.ElevationGain(), cadMax, cadAvg)
 }
 
 // represents each trackpoint
@@ -36,7 +43,7 @@ type TrackPoint struct {
   Lat         float64 `xml:"lat,attr"`
   Lon         float64 `xml:"lon,attr"`
   Elevation   float32 `xml:"ele"`
-  Time        string `xml:"time"`
+  Time        time.Time `xml:"time"`
   Temperature int `xml:"extensions>TrackPointExtension>atemp"`
   HeartRate   int `xml:"extensions>TrackPointExtension>hr"`
   Cadence     int `xml:"extensions>TrackPointExtension>cad"`
@@ -48,25 +55,52 @@ func (t TrackPoint) String() string {
 }
 
 // get the maximum heart rate
-func (g Gpx) MaxHeartRate() (hr int) {
-  hr = 0
-  for _, pt := range g.TrackPoints {
-    if pt.HeartRate > hr {
-      hr = pt.HeartRate
-    }
-  }
-  return
-}
-
-// get the average heart rate
-func (g Gpx) AverageHeartRate() (hr int) {
-
+func (g Gpx) HeartRate() (max, avg int) {
+  max = 0
   total := 0
   for _, pt := range g.TrackPoints {
     total += pt.HeartRate
+    if pt.HeartRate > max {
+      max = pt.HeartRate
+    }
+  }
+  avg = total / len(g.TrackPoints)
+  return
+}
+
+func (g Gpx) ElevationGain() (gain float32) {
+
+  el := g.TrackPoints[0].Elevation
+
+  for i := 1; i < len(g.TrackPoints); i++ {
+    ev := g.TrackPoints[i].Elevation
+    if ev > el {
+      gain += ev - el
+    }
+    el = ev
   }
 
-  hr = total/len(g.TrackPoints)
+  return
+}
+
+// Get the cadence stats.
+// returns the average and max values
+func (g Gpx) Cadence() (avg, max int) {
+
+  count, total := 0, 0
+  for _, pt := range g.TrackPoints {
+    if pt.Cadence > 0 {
+      count += 1
+      total += pt.Cadence
+    }
+
+    if pt.Cadence > max {
+      max = pt.Cadence
+    }
+  }
+
+  avg = total / count
+
   return
 }
 
